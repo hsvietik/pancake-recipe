561d7a2d0637a6a4daa9ace4afadc052282fd2a9
Hanna Svietik <hsvietik@gmail.com>
2022-08-01 22:25:08 +0100

    created

---
+++ script.js
@@ -0,0 +1,12 @@
+function ingredientsHover() {
+document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "200%"
+}
+function ingredientsNormal() {
+document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "100%"
+}
+function preparationHover() {
+document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = "200%"
+}
+function preparationNormal() {
+document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = "100%"
+}
